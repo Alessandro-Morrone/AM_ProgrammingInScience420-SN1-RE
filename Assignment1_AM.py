2756004222956d0b8009b058b5dd64bb029789b7
Alessandro-Morrone <6293368@edu.vaniercollege.qc.ca>
2025-10-01 23:11:25 -0400

    Update Assignment1_AM.py

--- Assignment1_AM.py
+++ Assignment1_AM.py
@@ -39,18 +39,33 @@ print(check_number(number))
 # Function 4: For Loop – Making a Star Shape
 # This function should return a string representing a star shape.
 def star_shape(rows):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    total_rows = range(0, rows+1)
+    for i in total_rows:
+        print("*"*i)
+
+print("Question 4: Making a Star Shape")
+rows = int(input("Enter the number of rows: "))
+star_shape(rows)
 
 # Function 5: While Loop – Counting Multiples of 3
 # This function should return a list of numbers from 1 to limit, replacing multiples of 3 with "Multiple of 3".
 def count_multiples_of_3(limit):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    count_numbers = range(0, limit+1)                #PROBLEM HERE IN FUNCTION
+    n = 0
+    while n <= limit:
+        if n % 3 ==0:
+            print("Multiple of 3")
+        else:
+            print(n)
+
+print("Question 5: Counting Multiples of 3")
+limit = int(input("Enter your endpoint: "))
+count_multiples_of_3(limit)
 
 # Function 6: Sum of Even Numbers in a Range
 # This function should calculate and return the sum of even numbers within a given range.
 def sum_of_even_numbers(start, end):
     # TODO: Implement this function
     pass  # Replace with your code
+
 
